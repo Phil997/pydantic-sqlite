914f1cbab3497c06e81b419c4a4b8248e0a3dc5c
Philipp Jaschke <p.jaschke@ewetel.net>
2025-07-08 21:05:37 +0200

    refactor: misc functions

--- pydantic_sqlite/_misc.py
+++ pydantic_sqlite/_misc.py
@@ -3,22 +3,40 @@ from typing import Any, get_args
 
 
 def get_unique_filename(filename: str) -> str:
-    name, ending = os.path.splitext(filename)
+    """
+    Generate a unique filename by appending a counter if the file already exists.
+
+    Args:
+        filename (str): The desired filename.
+
+    Returns:
+        str: A unique filename that does not exist in the current directory.
+    """
+    name, ext = os.path.splitext(filename)
     counter = 1
-
-    while os.path.exists(filename):
-        filename = f"{name}({str(counter)}){ending}"
+    unique_filename = filename
+    while os.path.exists(unique_filename):
+        unique_filename = f"{name}({counter}){ext}"
         counter += 1
-
-    return filename
+    return unique_filename
 
 
 def convert_value_into_union_types(union_type: Any, value: Any) -> Any:
+    """
+    Attempt to convert a value to one of the types in a Union type.
+
+    Args:
+        union_type (Any): The Union type to attempt conversion to.
+        value (Any): The value to convert.
+
+    Returns:
+        Any: The value converted to the first matching type in the Union, or the original value if no conversion succeeds.
+    """
     if type(None) in get_args(union_type) and value is None:
         return None
     for t in get_args(union_type):
         try:
             return t(value)
         except Exception:
-            ...
+            continue
     return value
